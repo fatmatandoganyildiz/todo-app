ab66a529c9a3c0670e77d5772d6c26d567a22b91
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-10-29 21:36:05 +0000

    Branch-1/First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import "./App.css";
+import React, { useState } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Fatma's Todo List</h1>
+      </header>
+      <Form
+        setInputText={setInputText}
+        inputText={inputText}
+        setTodos={setTodos}
+        todos={todos}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
